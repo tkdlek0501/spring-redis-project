60da9547da825a3116d2dd4adeec5ca521f5777d
khg6422 <khg6422@naver.com>
2025-01-21 19:14:07 +0900

    first commit

---
+++ src/main/java/com/example/redisinspring/BoardService.java
@@ -0,0 +1,26 @@
+package com.example.redisinspring;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+}
